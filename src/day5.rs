90a2cc833735014bc2772851ba84e5675826fcbd
Sebastian Alejandro <alejansw@rose-hulman.edu>
2022-12-06 00:14:54 -0500

    d5: minor comment change

--- src/day5.rs
+++ src/day5.rs
@@ -6,17 +6,15 @@ pub struct CrateMover {
 
 #[aoc_generator(day5)]
 pub fn input_generator(input: &str) -> CrateMover {
-    /*
-                        [Q]     [P] [P]
-                    [G] [V] [S] [Z] [F]
-                [W] [V] [F] [Z] [W] [Q]
-            [V] [T] [N] [J] [W] [B] [W]
-        [Z] [L] [V] [B] [C] [R] [N] [M]
-    [C] [W] [R] [H] [H] [P] [T] [M] [B]
-    [Q] [Q] [M] [Z] [Z] [N] [G] [G] [J]
-    [B] [R] [B] [C] [D] [H] [D] [C] [N]
-    1   2   3   4   5   6   7   8   9
-    */
+    //                     [Q]     [P] [P]
+    //                 [G] [V] [S] [Z] [F]
+    //             [W] [V] [F] [Z] [W] [Q]
+    //         [V] [T] [N] [J] [W] [B] [W]
+    //     [Z] [L] [V] [B] [C] [R] [N] [M]
+    // [C] [W] [R] [H] [H] [P] [T] [M] [B]
+    // [Q] [Q] [M] [Z] [Z] [N] [G] [G] [J]
+    // [B] [R] [B] [C] [D] [H] [D] [C] [N]
+    //  1   2   3   4   5   6   7   8   9
 
     let crates = vec![
         vec!['B', 'Q', 'C'],
